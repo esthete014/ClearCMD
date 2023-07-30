3f42ba3244b4173e93f702e1a157d10d3fdbd7b7
esthete014 <nikkochetov@outlook.com>
2023-07-31 00:14:58 +0300

    Добавьте файлы проекта.

---
+++ menu/menu/block.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+class TYPE {
+public:
+	std::string str = "";
+
+};
+
+
+template <class TYPE>
+class Block {
+public:
+	TYPE type = 0;
+	int vpoint = 1;
+	int hpoint = 1;
+
+};
---
+++ menu/menu/visual.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "block.h"
+
+class Visual {
+public:
+	Visual(std::vector<std::vector<Block<TYPE>>> layout) {
+		for (int i = 0; i < layout.size(); i++) {
+			for (int j = 0; j < layout[i].size(); j++) {
+				layout[i][j].type
+			}
+		}
+	}
+private:
+
+
+};
